ef66b02a0448704a09fd19aad2fcee29894510a1
Renato Cavalcante <115951188+Renato0001@users.noreply.github.com>
2022-10-18 08:57:36 -0300

    Add files via upload

---
+++ tab.py
@@ -0,0 +1,6 @@
+from tabuada import Tabuada
+
+
+todas_tabuada = Tabuada()
+
+todas_tabuada.calcular_tabuada()
---
+++ tabuada.py
@@ -0,0 +1,44 @@
+
+class Tabuada:
+    
+
+
+  def __init__(self):
+    multiplicacao = 1
+
+
+  def calcular_tabuada(self):
+
+    numero = int(input("Qual a tabuada desejar? "))
+    for self.multiplicao in range(1, 11):
+      self.multiplicao *= 1
+      print(self.multiplicao * numero)
+    print('')
+
+    numero = int(input("Qual a tabuada desejar? "))
+    for self.multiplicao in range(1, 11):
+      self.multiplicao *= 1
+      print(self.multiplicao * numero)
+    print('')
+
+    numero = int(input("Qual a tabuada desejar? "))
+    for self.multiplicao in range(1, 11):
+      self.multiplicao *= 1
+      print(self.multiplicao * numero)
+    print('')
+
+    numero = int(input("Qual a tabuada desejar? "))
+    for self.multiplicao in range(1, 11):
+      self.multiplicao *= 1
+      print(self.multiplicao * numero)
+    print('')
+
+    numero = int(input("Qual a tabuada desejar? "))
+    for self.multiplicao in range(1, 11):
+      self.multiplicao *= 1
+      print(self.multiplicao * numero)
+    print('')
+       
+       
+
+   
