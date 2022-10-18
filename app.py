21baf782194de155ed2aba5834d6a075b0f2da2d
Renato Cavalcante <115951188+Renato0001@users.noreply.github.com>
2022-10-18 09:37:29 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-from figura_geometrica import FiguraGeometrica
-triangulo = FiguraGeometrica(3, 4, "triangulo")
-quadrado = FiguraGeometrica(3, 3, "quadrado")
-retangulo = FiguraGeometrica(10, 5, "")
+from reajuste_salario import  ReajusteSalarial
 
-print("O valor da área do triangulo é: ", triangulo.calcular_area())
-print("O valor da área do quadrado é: ", quadrado.calcular_area())
-print("O valor da área do retangulo é: ", retangulo.calcular_area())
+calculo_do_reajuste = ReajusteSalarial(699, 0.15)
 
-print("O valor da altura antes da mudamça: ", triangulo.get_altura())
-#triangulo.set_lado(2)
-#triangulo.set_altura(5)
-#print("O valor da altura após a mudança: ", triangulo.get_altura())
-#area = (triangulo.get_altura(3) * triangulo.get_lado(3)) / 2
-#print("O valor da area é: ", area)
+calculo_do_reajuste.mostrar_salario()
+calculo_do_reajuste.mostrar_reajuste()
+calculo_do_reajuste.calcular_reajuste()
+calculo_do_reajuste.mostrar_aumento()
